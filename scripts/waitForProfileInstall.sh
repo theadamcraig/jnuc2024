be55a2f49fcf83cb65c787076df4ece22cbd285f
Adam Caudill <acaudill@hubspot.com>
2024-07-23 13:11:28 -0400

    adding some functions

---
+++ scripts/waitForProfileInstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+profileName="${4}"
+
+function waitForProfileInstall () {
+    index=0
+    while [[ "$(profiles -C -v | grep "$profileName" | awk -F": " '/attribute: name/{print $NF}')" != *"$profileName"* ]] && [ $index -le 25 ] ; do
+        echo "Waiting for $profileName configuration profile to install..."
+        sleep 5
+        (( index++ ))
+    done
+    
+    if [[ "$(profiles -C -v | grep "$profileName" | awk -F": " '/attribute: name/{print $NF}')" ==  *"$profileName"* ]]; then
+        echo "$profileName configuration profile has been installed"
+    else
+        echo "Profile was still not installed. failing."
+        #displaynotification "Certificate Profile failed to Install!" "Troubleshooting"
+        exit 1
+    fi
+}
