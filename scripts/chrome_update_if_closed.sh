20fed157426673edc58e36985642222d09e550ae
Adam Caudill <adamcraig@me.com>
2024-08-26 14:43:58 -0400

    Update chrome_update_if_closed.sh

--- scripts/chrome_update_if_closed.sh
+++ scripts/chrome_update_if_closed.sh
@@ -3,10 +3,29 @@
 applicationName="Google Chrome"
 updateTrigger="install_chrome"
 
+policyTriggerCorrectResults() { 
+    policyTrigger="${1}"
+    echo "Running the jamf policy ${policyTrigger}"
+    result=$( jamf policy -event "${policyTrigger}" -forceNoRecon )
+    exitCode="$?"
+    echo "Result of jamf policy is: ${exitCode}"
+    if [[ "$result" == *"No policies were found"* ]] ; then
+        echo "$result"
+        echo "setting exit code to 1"
+        exitCode=1
+    fi
+    echo "Script result is $result"
+    
+    if [[ "${exitCode}" = 0 ]] ; then
+        echo "Policy run successfully"
+    else
+        echo "Policy either failed or was not found."
+    fi
+}
 
 if [[ -z $( pgrep -x "$applicationName" ) ]] ; then
     echo "$applicationName is not running."
-    jamf policy -event $updateTrigger -forceNoRecon
+    policyTriggerCorrectResults "${updateTrigger}"
     jamf recon
 else
     echo "$applicationName is currently running"
