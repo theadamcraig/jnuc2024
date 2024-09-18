2f7d9c8d508f3730f1e5bacf0df056241b27cd2b
Adam Caudill <adamcraig@me.com>
2024-09-18 11:46:07 -0400

    cleaned up

--- scripts/patch_group - EA.sh
+++ scripts/patch_group - EA.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-#Clean up old EA
-if [[ -d "/Library/Management/patch-groups" ]]; then
-    rm -rf "/Library/Management/patch-groups"
-fi
+companyName="YourCompanyNameHere"
 
 # Get today's date in YYYY-MM-DD format
 current_date=$(date +"%Y-%m-%d")
 
 # Create plist file if it doesn't exist or if current_group key doesn't exist
-if [[ ! -f "/Library/Preferences/com.hubspot.patchgroup.plist" ]] || ! defaults read "/Library/Preferences/com.hubspot.patchgroup.plist" current_group &>/dev/null; then
-    touch "/Library/Preferences/com.hubspot.patchgroup.plist"
-    defaults write "/Library/Preferences/com.hubspot.patchgroup.plist" current_group -int 0
+if [[ ! -f "/Library/Preferences/com.${companyName}.patchgroup.plist" ]] || ! defaults read "/Library/Preferences/com.${companyName}.patchgroup.plist" current_group &>/dev/null; then
+    touch "/Library/Preferences/com.${companyName}.patchgroup.plist"
+    defaults write "/Library/Preferences/com.${companyName}.patchgroup.plist" current_group -int 0
 fi
 
 # Read the last updated date from the plist file
-last_updated=$(defaults read "/Library/Preferences/com.hubspot.patchgroup.plist" last_updated 2>/dev/null)
+last_updated=$(defaults read "/Library/Preferences/com.${companyName}.patchgroup.plist" last_updated 2>/dev/null)
 
 # If last_updated is today's date, exit the script
-if [[ "$last_updated" == "$current_date" ]] && [[ ! $(defaults read "/Library/Preferences/com.hubspot.patchgroup.plist" current_group) -eq 0 ]]; then
-    echo "<result>$(defaults read "/Library/Preferences/com.hubspot.patchgroup.plist" current_group)</result>"
+if [[ "$last_updated" == "$current_date" ]] && [[ ! $(defaults read "/Library/Preferences/com.${companyName}.patchgroup.plist" current_group) -eq 0 ]]; then
+    echo "<result>$(defaults read "/Library/Preferences/com.${companyName}.patchgroup.plist" current_group)</result>"
     exit 0
 fi
 
 # Read the current group from the plist file
-current_group=$(defaults read /Library/Preferences/com.hubspot.patchgroup.plist current_group)
+current_group=$(defaults read "/Library/Preferences/com.${companyName}.patchgroup.plist" current_group)
 
 # If the current group is empty or zero, randomly generate a number between 1-8 to set the value
 if [[ -z "$current_group" || "$current_group" -eq 0 ]]; then
@@ -39,11 +36,11 @@ else
 fi
 
 # Update the plist file with the new current_group value
-defaults write /Library/Preferences/com.hubspot.patchgroup.plist current_group -int $current_group
+defaults write "/Library/Preferences/com.${companyName}.patchgroup.plist" current_group -int $current_group
 
 # Update the plist file with the new last_updated date
-defaults write /Library/Preferences/com.hubspot.patchgroup.plist last_updated $current_date
+defaults write "/Library/Preferences/com.${companyName}.patchgroup.plist" last_updated $current_date
 
-echo "<result>$(defaults read "/Library/Preferences/com.hubspot.patchgroup.plist" current_group)</result>"
+echo "<result>$(defaults read "/Library/Preferences/com.${companyName}.patchgroup.plist" current_group)</result>"
 
 exit 0
